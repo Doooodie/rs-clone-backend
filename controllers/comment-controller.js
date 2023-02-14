028382b2902ca05a659638bb69bb76253cee4873
eys-man <112211574+eys-man@users.noreply.github.com>
2023-02-14 15:21:14 +0300

    Update comment-controller.js

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,11 +1,9 @@
 class CommentController {
     async create(req, res) {
-        // console.log('user-controller: login');
         res.json({ message: 'comment-controller: create'});
     }
 
     async delete(req, res) {
-        // console.log('user-controller: check');
         res.json({ message: 'comment-controller: delete'});
     }
 }
