84d2f71b14467ca112cd24e9336c8bed4b128fc1
eys-man <kapuec2010@gmail.com>
2023-03-01 23:52:25 +0300

    fix: static files path fixed

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(dirname, '/public')));
+app.use(express.static(path.resolve(dirname, '../public')));
 app.use(fileUpload({}));
 app.use('/', router);
 
